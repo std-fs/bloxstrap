514e096091e819c8e896ce9fbcf6e774e41193f0
std-fs <155996914+std-fs@users.noreply.github.com>
2024-02-09 14:06:27 +0800

    Update App.xaml.cs

--- Bloxstrap/App.xaml.cs
+++ Bloxstrap/App.xaml.cs
@@ -13,8 +13,8 @@ namespace Bloxstrap
     /// </summary>
     public partial class App : Application
     {
-        public const string ProjectName = "Bloxstrap";
-        public const string ProjectRepository = "pizzaboxer/bloxstrap";
+        public const string ProjectName = "STD-FS FORK";
+        public const string ProjectRepository = "Original Creator: pizzaboxer/bloxstrap";
         public const string RobloxPlayerAppName = "RobloxPlayerBeta";
         public const string RobloxStudioAppName = "RobloxStudioBeta";
 
